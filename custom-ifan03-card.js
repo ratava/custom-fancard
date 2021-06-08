2a0e0893f2abc0da0a62af46fa6ed08fe0d28398
ratava <brent.wesley11@gmail.com>
2021-06-09 06:26:15 +1000

    Update custom-ifan03-card.js

--- custom-ifan03-card.js
+++ custom-ifan03-card.js
@@ -129,28 +129,28 @@ class Card extends LitElement {
       }
       
       div[light="toggle"] {
-        mask-image: url('lightbulb.svg');
-        -webkit-mask-image: url(off.svg');
+        mask-image: url('/local/community/custom-fancard/lightbulb.svg');
+        -webkit-mask-image: url('/local/community/custom-fancard/off.svg');
       }
 			
       div[data-speed="off"] {
-        mask-image: url('off.svg');
-        -webkit-mask-image: url('off.svg');
+        mask-image: url('/local/community/custom-fancard/off.svg');
+        -webkit-mask-image: url('/local/community/custom-fancard/off.svg');
       }
       
       div[data-speed="low"] {
-        mask-image: url('low.svg');
-        -webkit-mask-image: url('low.svg');
+        mask-image: url('/local/community/custom-fancard/low.svg');
+        -webkit-mask-image: url('/local/community/custom-fancard/low.svg');
       }
       
       div[data-speed="medium"] {
-        mask-image: url('medium.svg');
-        -webkit-mask-image: url('medium.svg');
+        mask-image: url('/local/community/custom-fancard/medium.svg');
+        -webkit-mask-image: url('/local/community/custom-fancard/medium.svg');
       }
       
       div[data-speed="high"] {
-        mask-image: url('high.svg');
-        -webkit-mask-image: url('high.svg');
+        mask-image: url('/local/community/custom-fancard/high.svg');
+        -webkit-mask-image: url('/local/community/custom-fancard/high.svg');
       }
       
       .light div[light="toggle"],
